44ca4f202090d3a8245b4d409208486e00446b04
Baiz <b.mohamedachraf@gmail.com>
2018-02-16 13:39:47 +0100

    advancment

--- Python_OpenCV/main_process.py
+++ Python_OpenCV/main_process.py
@@ -12,12 +12,39 @@ from matplotlib import pyplot as plt
 
 
 def reconstitue():
-    for i in range(300,501):
-        img1 = cv.imread('output/frame%d.jpg'%i)
-        img2 = cv.imread('informations_output/image_%d.png'%i)
+    for i in range(560,1254):
+        img1 = cv.imread('output_presentation/frame%d.jpg'%i)
+        img2 = cv.imread('informations_output_presentation/image_%d.png'%i)
         x_offset=y_offset=50
-        img1[y_offset:y_offset+img2.shape[0], x_offset:x_offset+img2.shape[1]] = img2
-        cv.imwrite('final/out_%d.png'%i, img1)
+        imgf = face_recognection(img1)
+        imgf[y_offset:y_offset+img2.shape[0], x_offset:x_offset+img2.shape[1]] = img2
+        cv.imwrite('final_presentation/out_%d.png'%i, imgf)
+        
+def face_recognection(img):
+    #xml face shape recognition path
+    cascPath = "haarcascade_frontalface_default.xml"
+    # Create the haar cascade
+    faceCascade = cv.CascadeClassifier(cascPath)
+    
+    # Read the image
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv.CASCADE_SCALE_IMAGE    
+    )
+    
+    print("Found {0} faces!".format(len(faces)))
+    
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    return img
 
 def histogramme(img,cannaux,_id):
     if cannaux==3:
@@ -34,7 +61,7 @@ def histogramme(img,cannaux,_id):
         green_patch = mpatches.Patch(color='green', label='green intensity')
         plt.legend(handles=[red_patch,blue_patch,green_patch])
         '''
-        plt.savefig('informations_output/image_%d.png'%_id)
+        plt.savefig('informations_output_presentation/image_%d.png'%_id)
         plt.gcf().clear()
 def daylight(img):
     white_pix = 0
@@ -54,15 +81,29 @@ def daylight(img):
         print("afternon/early morning")
     elif(avg<0.40):
         print("day")
+        
+def video_images(path):
+    assert type(path)==str
+    vidcap = cv.VideoCapture(path)
+    success,image = vidcap.read()
+    count = 0
+    success = True
+    while success:
+      success,image = vidcap.read()
+      print('Read a new frame: ', success)
+      cv.imwrite("output_presentation/frame%d.jpg" % count, image)     # save frame as JPEG file
+      count += 1        
 
 if __name__=='__main__': 
-    
-    for i in range(300,501): # 100 images
-        img   = cv.imread(('output/frame%d.jpg'%i),1)
+    #video_images("accident.mp4")
+    '''
+    for i in range(560,1254): # 100 images
+        img   = cv.imread(('output_presentation/frame%d.jpg'%i),1)
         histogramme(img,3,i)
         daylight(img)
-    
+    '''
     reconstitue()
+    
     
     '''
     cv.namedWindow('img') 
